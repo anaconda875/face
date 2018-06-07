7e811505dfa2c9fcf9a809f560372d31aadbe649
Bao Ngo Nguyen Gia <nngbao@aavn.local>
2018-06-07 16:44:42 +0700

    Increase some filter

--- new.py
+++ new.py
@@ -66,7 +66,7 @@ def fc_layer(flatten, output_size, use_relu=True, name=None):
 			return tf.nn.relu(tf.add(tf.matmul(flatten, output), b), name='relu'+name[-1])
 		return tf.nn.softmax(tf.add(tf.matmul(flatten, output), b), name='softmax'+name[-1])
 		
-files, labels = read_dataset('/localhome/localuser/face/face')
+files, labels = read_dataset('C:\\Users\\nngbao\\Downloads\\face_triplet\\data\\face')
 a = DataSet(files, labels)
 t1 = a.get_files()
 t2 = a.get_labels()
@@ -80,21 +80,22 @@ with g.as_default():
 	y = tf.placeholder(tf.float32, shape=[None], name='real_label')
 	y_onehot = tf.placeholder(tf.float32, shape=[None, 10], name='onehot_real_label')
 	with tf.variable_scope('FEATURE_EXTRACTOR'):
-		conv_l1 = conv_layer(x, [5, 5, 3, 16], [1, 1, 1, 1], name='CONV_L1')
-		conv_l2 = conv_layer(conv_l1, [3, 3, 16, 32], [1, 1, 1, 1], name='CONV_L2')
+		conv_l1 = conv_layer(x, [5, 5, 3, 24], [1, 1, 1, 1], name='CONV_L1')
+		conv_l2 = conv_layer(conv_l1, [3, 3, 24, 48], [1, 1, 1, 1], name='CONV_L2')
 		#conv_l3 = conv_layer(conv_l2, [3, 3, 32, 32], [1, 1, 1, 1], name='CONV_L3')
-		conv_l3 = conv_layer(conv_l2, [2, 2, 32, 56], [1, 1, 1, 1], name='CONV_L3')
-		conv_l4 = conv_layer(conv_l3, [2, 2, 56, 72], [1, 1, 1, 1], name='CONV_L4')
+		conv_l3 = conv_layer(conv_l2, [2, 2, 48, 72], [1, 1, 1, 1], name='CONV_L3')
+		conv_l4 = conv_layer(conv_l3, [2, 2, 72, 180], [1, 1, 1, 1], name='CONV_L4')
 		flatten = flatten_layer(conv_l4, 'FLATTEN_L')
-		fc_l1 = fc_layer(flatten, [2592, 1024], name='FC_L1')
+		fc_l1 = fc_layer(flatten, [6480, 1536], name='FC_L1')
 		keep_prob = tf.placeholder(tf.float32)
 		drop_l = tf.nn.dropout(fc_l1, keep_prob)
-		fc_l2 = fc_layer(drop_l, [1024, 256], name='FC_L2')
+		fc_l2 = fc_layer(drop_l, [1536, 512], name='FC_L2')
 		l2_norm_l = tf.nn.l2_normalize(fc_l2, axis=1)
 
 	with tf.variable_scope('CLASSIFIER'):
-		fc_l3 = fc_layer(fc_l2, [256, 192], name='FC_L3')
-		softmax = fc_layer(fc_l3, [192, 10], False, 'FC_SOFTMAX_L4')
+		fc_l3 = fc_layer(fc_l2, [512, 256], name='FC_L3')
+		softmax = fc_layer(fc_l3, [256, 10], False, 'FC_SOFTMAX_L4')
+		print(softmax)
 	
 	triplet_loss = tf.contrib.losses.metric_learning.triplet_semihard_loss(y, l2_norm_l)
 	cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_onehot, logits=softmax))
@@ -104,15 +105,15 @@ with g.as_default():
 	second_train_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "CLASSIFIER")
 	
 	global_step = tf.Variable(0, trainable=False)
-	learning_rate = tf.where(tf.greater_equal(global_step, 10000),
-							tf.train.polynomial_decay(1e-4, global_step,
-														18000, 4e-6,
+	learning_rate = tf.where(tf.greater_equal(global_step, 8000),
+							tf.train.polynomial_decay(1e-3, global_step,
+														8000, 4e-6,
 														power=1.0),
 							1e-4)
 	tf.summary.scalar('learning_rate', learning_rate)
 	
 	first_train_ops = tf.train.AdamOptimizer(learning_rate).minimize(triplet_loss, global_step=global_step, var_list=first_train_vars)
-	second_train_ops = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy_loss, global_step=global_step, var_list=second_train_vars)
+	second_train_ops = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy_loss, var_list=second_train_vars)
 	
 	tf.summary.scalar("triplet_loss", triplet_loss)
 	tf.summary.scalar("cross_entropy_loss", cross_entropy_loss)
@@ -120,18 +121,18 @@ with g.as_default():
 	
 	merged_summary_op = tf.summary.merge_all()
 	
-	summary_writer = tf.summary.FileWriter('/localhome/localuser/ml/tmp', graph=tf.get_default_graph())
+	summary_writer = tf.summary.FileWriter('/tmp', graph=tf.get_default_graph())
 	saver = tf.train.Saver()
 	
 	sess = tf.Session()
 	sess.run(tf.global_variables_initializer())
 	
 	
-	for step in range (30000):
-		batch_x, batch_y = a.next_batch(50)
+	for step in range (28000):
+		batch_x, batch_y = a.next_batch(40)
 		_x = []
 		_y_onehot = []
-		for j in range (50):
+		for j in range (40):
 			onehot = np.zeros(10)
 			onehot[batch_y[j]] = 1
 			_y_onehot.append(onehot)
@@ -141,16 +142,16 @@ with g.as_default():
 		batch_y = np.asarray(batch_y)
 		_y_onehot = np.asarray(_y_onehot)
 
-		_, _, _triplet_loss, _cross_entropy_loss, summary = sess.run([first_train_ops, second_train_ops, triplet_loss, cross_entropy_loss, merged_summary_op], feed_dict={x: batch_x, y: batch_y, y_onehot: _y_onehot, keep_prob: 0.5})
+		_, _, _triplet_loss, _cross_entropy_loss, summary = sess.run([first_train_ops, second_train_ops, triplet_loss, cross_entropy_loss, merged_summary_op], feed_dict={x: batch_x, y: batch_y, y_onehot: _y_onehot, keep_prob: 0.55})
 		if step % 100 == 0:
 			train_accuracy = sess.run([accuracy], feed_dict={x: batch_x, y_onehot: _y_onehot, keep_prob: 1.})
 			#train_accuracy = accuracy.eval({x: batch_x, y: batch_y, keep_prob: 1.0}, sess)
-			print('step %d, triplet_loss %f, cross_entropy_loss %f, total_loss %f, accuracy %f' % (step, _triplet_loss, _cross_entropy_loss, (_triplet_loss + _cross_entropy_loss), train_accuracy[-1]))
-#			print('step %d, triplet_loss %f' % (step, _triplet_loss))
+			print('global_step %s, triplet_loss %.4f, cross_entropy_loss %.4f, total_loss %.4f, accuracy %.2f' % (tf.train.global_step(sess, global_step), _triplet_loss, _cross_entropy_loss, (_triplet_loss + _cross_entropy_loss), train_accuracy[-1]))
+#			print(', triplet_loss %f' % (step, _triplet_loss))
 			summary_writer.add_summary(summary, step)
 			summary_writer.flush()
 		
-	save_path = saver.save(sess, '/localhome/localuser/ml/model' + "/model.ckpt", global_step=step)
+	save_path = saver.save(sess, '/tmp/model' + "/model.ckpt", global_step=step)
 	#logging.info("Model saved in file: %s" % save_path)
 	
 	'''print(conv_l1)
